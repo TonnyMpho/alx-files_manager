0a12bc308dba68663ffbf8c56f15d3082c82ed2e
TonnyMpho <tonnympho1@gmail.com>
2024-03-11 11:43:21 +0000

    Files Manager

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.log(`Redis Client Error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((res, rej) => {
+      this.client.get(key, (err, reply) => {
+	if (err) {
+	  rej(err);
+	} else {
+	  res(reply);
+	}
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+	  reject(err);
+	} else {
+	  resolve(reply);
+	}
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+	if (err) {
+	  reject(err);
+	} else {
+	  resolve(reply);
+	}
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
